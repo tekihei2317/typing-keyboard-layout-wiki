657eac838c8a87d7beb3dad8c3afbb5bf63e4cdd
tekihei2317 <tekihei2317@gmail.com>
2025-09-30 19:52:36 +0900

    顔の慣用句

--- e-typing-analyzer/generate-analysis.ts
+++ e-typing-analyzer/generate-analysis.ts
@@ -6,7 +6,7 @@ import {
 async function generateAnalysis() {
   try {
     const analysis = await loadAndAnalyzeWords(
-      "../words/e-typing/ことわざ.xml"
+      "../words/e-typing/顔の慣用句.xml"
     );
     const keyboardData = getKeyboardVisualizationData(analysis);
 
